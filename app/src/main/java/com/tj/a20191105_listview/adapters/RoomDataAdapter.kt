e67b129e55b02996368249d0bb27ce7bdf06837b
mikemskim <mskim@steconsulting.co.kr>
2019-11-05 21:09:10 +0900

    어댑터 생성 연습  - 클래스의 기본 생성자 다시 활용  - 상속 : ArrayAdapter<RoomData>  - 상속시 부모의 생성자중 하나에, 이 클래스의 기본 생성자가 받아오는 파라미터를 전달  - 자주 쓰는 변수 3개 생성, mContext, mList, inf  - init을 통해서 세개 변수에 데이터 초기화  - getView를 이용해 재사용 여부에 대한 대응 준비    => 원하는 대로 데이터를 집어넣기 위한 메소드

---
+++ app/src/main/java/com/tj/a20191105_listview/adapters/RoomDataAdapter.kt
@@ -0,0 +1,32 @@
+package com.tj.a20191105_listview.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.tj.a20191105_listview.R
+import com.tj.a20191105_listview.data.RoomData
+
+class RoomDataAdapter (context: Context, resource:Int, list:ArrayList<RoomData>)
+    : ArrayAdapter<RoomData>(context, resource, list){
+
+    var mContext:Context? = null
+    var mList:ArrayList<RoomData>? = null
+    var inf:LayoutInflater? = null
+
+    init {
+        mContext = context
+        mList = list
+        inf = LayoutInflater.from(mContext)
+    }
+
+    // 한줄을 그릴때 마다 호출함 Position이 0부터
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var row = convertView
+        if (row == null) {
+            row = inf?.inflate(R.layout.room_list_item, null)
+        }
+
+    }
+}
