ff92541f9dd6921deecd2d14151d2ba43fd4d96d
mikemskim <mskim@steconsulting.co.kr>
2019-11-05 21:39:43 +0900

    새 액티비티 생성, 리스트뷰 생성

---
+++ app/src/main/java/com/tj/a20191105_listview/ListViewPracticeActivity.kt
@@ -0,0 +1,12 @@
+package com.tj.a20191105_listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListViewPracticeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view_practice)
+    }
+}
