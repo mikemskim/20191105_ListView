651c63c4a0628501ea06d6f167e648d298372c89
mikemskim <mskim@steconsulting.co.kr>
2019-11-05 22:19:54 +0900

    어뎁터의 getView에서 상황에 맞는 데이터를 출력 수정

--- app/src/main/java/com/tj/a20191105_listview/data/PostData.kt
+++ app/src/main/java/com/tj/a20191105_listview/data/PostData.kt
@@ -1,6 +1,12 @@
 package com.tj.a20191105_listview.data
 
+import android.util.Log
+
 class PostData(inputTitle:String, inputContent:String) {
     var title = inputTitle
     var content = inputContent
+
+    init {
+        Log.d("게시글생성", "기본생성자로 생성")
+    }
 }
